8e81132864ea2e829d55ab89a92707060cd771cf
SuJiehao <megasu@qq.com>
2023-06-16 15:56:23 +0800

    feat: 封装文章项组件

--- pages/article.vue
+++ pages/article.vue
@@ -10,29 +10,8 @@
         <a href="javascript:;">最新</a>
         <div class="logo"><img src="~/assets/logo.png" alt="" /></div>
       </nav>
-
-      <van-cell class="article-item">
-        <template #title>
-          <div class="head">
-            <img
-              src="http://teachoss.itheima.net/heimaQuestionMiniapp/%E5%AE%98%E6%96%B9%E9%BB%98%E8%AE%A4%E5%A4%B4%E5%83%8F%402x.png"
-              alt=""
-            />
-            <div class="con">
-              <p class="title van-ellipsis">宇宙头条校招前端面经</p>
-              <p class="other">不风流怎样倜傥 | 2022-01-20 00-00-00</p>
-            </div>
-          </div>
-        </template>
-        <template #label>
-          <div class="body van-multi-ellipsis--l2">
-            笔者读大三, 前端小白一枚, 正在准备春招, 人生第一次面试,
-            投了头条前端, 总共经历了四轮技术面试和一轮hr面, 不多说,
-            直接上题&nbsp;一面
-          </div>
-          <div class="foot">点赞 46 | 浏览 332</div>
-        </template>
-      </van-cell>
+      <!-- 文章项组件 - 自动导入 -->
+      <ArticleItem />
     </div>
   </NuxtLayout>
 </template>
