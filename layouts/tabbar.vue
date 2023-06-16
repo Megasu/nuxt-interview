796af9845cee54297b15df6443045d76cf109033
SuJiehao <megasu@qq.com>
2023-06-16 13:26:59 +0800

    feat: tabBar 路由模式

--- layouts/tabbar.vue
+++ layouts/tabbar.vue
@@ -1,12 +1,11 @@
 <template>
   <div class="layout-page">
-    首页架子 - 内容区域
     <slot />
-    <van-tabbar>
-      <van-tabbar-item icon="notes-o">面经</van-tabbar-item>
-      <van-tabbar-item icon="star-o">收藏</van-tabbar-item>
-      <van-tabbar-item icon="like-o">喜欢</van-tabbar-item>
-      <van-tabbar-item icon="user-o">我的</van-tabbar-item>
+    <van-tabbar route>
+      <van-tabbar-item to="/article" icon="notes-o">面经</van-tabbar-item>
+      <van-tabbar-item to="/collect" icon="star-o">收藏</van-tabbar-item>
+      <van-tabbar-item to="/like" icon="like-o">喜欢</van-tabbar-item>
+      <van-tabbar-item to="/user" icon="user-o">我的</van-tabbar-item>
     </van-tabbar>
   </div>
 </template>
