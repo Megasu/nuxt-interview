0d2c00ab04f71b986eacce60710d23c68d296234
SuJiehao <megasu@qq.com>
2023-06-16 12:05:49 +0800

    feat: Nuxt 路由基础

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <!-- 路由链接 -->
+  <NuxtLink to="/">首页</NuxtLink>
+  <NuxtLink to="/about">关于</NuxtLink>
+  <!-- 路由占位 -->
+  <NuxtPage />
 </template>
