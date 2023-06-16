4b6a01ea21b290de5680451ffad5ed87c5e0f868
SuJiehao <megasu@qq.com>
2023-06-16 13:24:00 +0800

    feat: 移动端适配 vw

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,14 @@
 export default defineNuxtConfig({
   devtools: { enabled: false },
   modules: ['@vant/nuxt'],
+  // 移动端适配
+  // https://nuxt.com/docs/api/configuration/nuxt-config#postcss
+  postcss: {
+    // https://vant-ui.github.io/vant/#/zh-CN/advanced-usage#liu-lan-qi-gua-pei
+    plugins: {
+      'postcss-px-to-viewport': {
+        viewportWidth: 375,
+      },
+    },
+  },
 })
