7d2e55444cac701f462916c1693a207371f4974c
SuJiehao <megasu@qq.com>
2023-06-16 13:39:56 +0800

    feat: 静态结构-注册页

--- pages/register.vue
+++ pages/register.vue
@@ -1,11 +1,51 @@
 <script setup lang="ts">
+const username = ref('itheima')
+const password = ref('123456')
 
+const onSubmit = (values: any) => {
+  console.log('submit', values)
+}
 </script>
 
 <template>
-  <h1>register</h1>
+  <div class="login-page">
+    <van-nav-bar title="面经注册" />
+
+    <van-form @submit="onSubmit">
+      <van-field
+        v-model="username"
+        name="username"
+        label="用户名"
+        placeholder="用户名"
+        :rules="[
+          { required: true, message: '请填写用户名' },
+          { pattern: /^\w{5,}$/, message: '用户名至少包含5个字符' },
+        ]"
+      />
+      <van-field
+        v-model="password"
+        type="password"
+        name="password"
+        label="密码"
+        placeholder="密码"
+        :rules="[
+          { required: true, message: '请填写密码' },
+          { pattern: /^\w{6,}$/, message: '密码至少包含6个字符' },
+        ]"
+      />
+      <div style="margin: 16px">
+        <van-button block type="primary" native-type="submit">注册</van-button>
+      </div>
+    </van-form>
+    <NuxtLink class="link" to="/login">有账号，去登录</NuxtLink>
+  </div>
 </template>
 
 <style scoped>
-
+.link {
+  color: #069;
+  font-size: 12px;
+  padding-right: 20px;
+  float: right;
+}
 </style>
