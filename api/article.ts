ee271e83b4448ee0cc380c6d4380e729e9646486
Mega Su <Megasu@qq.com>
2023-06-21 11:51:32 +0800

    feat: 我的喜欢+我的收藏

--- api/article.ts
+++ api/article.ts
@@ -7,3 +7,25 @@ export const updateLikeAPI = (id: string) => {
 export const updateCollectAPI = (id: string) => {
   return request.post('interview/opt', { id, optType: 2 })
 }
+
+// 获取收藏列表
+export const getArticlesCollect = (page: number) => {
+  return request.get('/interview/opt/list', {
+    params: {
+      page, // 当前页
+      pageSize: 5, // 页容量
+      optType: 2, // 表示收藏
+    },
+  })
+}
+
+// 获取喜欢列表
+export const getArticlesLike = (page: number) => {
+  return request.get('/interview/opt/list', {
+    params: {
+      page, // 当前页
+      pageSize: 5, // 页容量
+      optType: 1, // 表示喜欢
+    },
+  })
+}
--- pages/like.vue
+++ pages/like.vue
@@ -1,13 +1,43 @@
 <script setup lang="ts">
+import { getArticlesLike } from '~/api/article'
+import { ref } from 'vue'
 
+const list = ref<any[]>([])
+const loading = ref(false)
+const finished = ref(false)
+const page = ref(1)
+
+const onLoad = async () => {
+  // 异步更新数据
+  const { data } = await getArticlesLike(page.value)
+  list.value.push(...data.rows)
+  loading.value = false
+  page.value++
+  if (page.value > data.pageTotal) {
+    finished.value = true
+  }
+}
 </script>
 
 <template>
   <NuxtLayout name="tabbar">
-    <h1>like</h1>
+    <div class="like-page">
+      <van-nav-bar fixed title="我的点赞" />
+      <van-list
+        v-model:loading="loading"
+        :finished="finished"
+        finished-text="没有更多了"
+        @load="onLoad"
+      >
+        <ArticleItem v-for="(item, i) in list" :key="i" :item="item" />
+      </van-list>
+    </div>
   </NuxtLayout>
 </template>
 
-<style scoped>
-
+<style lang="less" scoped>
+.like-page {
+  margin-bottom: 50px;
+  margin-top: 44px;
+}
 </style>
